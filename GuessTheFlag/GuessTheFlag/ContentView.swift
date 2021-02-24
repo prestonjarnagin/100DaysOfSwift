b1084d53ab486022241a8048100636571889d8a7
Preston Jarnagin <preston@homebot.ai>
2021-02-23 18:17:09 -0700

    Add initial GuessTheFlag project structure

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Preston Jarnagin on 2/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack() {
+            HStack() {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            HStack() {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            HStack() {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+        }
+        VStack() {
+            HStack() {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            HStack() {
+                Text("Hello, world!")
+            }
+            HStack() {
+                Text("Hello, world!")
+            }
+        }
+        VStack() {
+            HStack() {
+                Text("Hello, world!")
+            }
+            HStack() {
+                Text("Hello, world!")
+            }
+            HStack() {
+                Text("Hello, world!")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Preston Jarnagin on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
