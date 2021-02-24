7ddc3dd68dbe2f0ba231a932af5bba1e28aa0b30
Preston Jarnagin <preston@homebot.ai>
2021-02-23 18:41:28 -0700

    Add flag buttons and background

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,42 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco",  "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
     var body: some View {
-        VStack() {
-            HStack() {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-            HStack() {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-            HStack() {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-        }
-        VStack() {
-            HStack() {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-            HStack() {
-                Text("Hello, world!")
-            }
-            HStack() {
-                Text("Hello, world!")
-            }
-        }
-        VStack() {
-            HStack() {
-                Text("Hello, world!")
-            }
-            HStack() {
-                Text("Hello, world!")
-            }
-            HStack() {
-                Text("Hello, world!")
+        ZStack{
+            Color.blue.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        //flag was tapped
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+                
+                Spacer()
             }
         }
     }
