73c7e4d3822b4ca0f1f7489e0118306af4d7d3e8
Preston Jarnagin <preston@homebot.ai>
2021-03-02 16:05:18 -0700

    Add rock paper scissors

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Preston Jarnagin on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
