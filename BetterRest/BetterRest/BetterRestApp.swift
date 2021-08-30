f4f2ae298018c78c2552436b38cb91fc7a2dd503
Preston Jarnagin <preston@homebot.ai>
2021-08-30 14:27:34 -0600

    Add BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Preston Jarnagin on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
